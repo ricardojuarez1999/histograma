618b782068ea909dd1b70ff76d9e0d2b7f9dcfdf
ricardojuarez1999 <ricardojuarez1999@gmail.com>
2017-05-24 23:50:58 -0600

    hola

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-number = getElementById("number");
-btnAdd = getElementById("agregar");
-btnHist = getElementById("hist");
+txtnombres = document.getElementById("number");
+btnAdd = document.getElementById("agregar");
+btnHist = document.getElementById("hist");
 
-function runScript(e) {
-    if (e.keyCode == 13) {
-        var tb = document.getElementById("scriptBox");
-        eval(tb.value);
-        return false;
-    }
+
+
+
+function myFunction(event) {
+	var x = event.which || event.KeyCode;
+	document.getElementById("demo").innerHTML = "The Unicode value is: " + x;
+}
