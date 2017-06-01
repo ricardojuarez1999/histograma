41a16e7e32e20333b43135c78be72af8fcaaa5a7
ricardojuarez <ricardojuarez1999@gmail.com>
2017-06-01 11:44:42 -0600

    histograma

--- scripts.js
+++ scripts.js
@@ -1,23 +1,36 @@
-<<<<<<< HEAD
+number = document.getElementById("number");
 btnAdd = document.getElementById("agregar");
 btnHist = document.getElementById("hist");
 numeros = document.getElementById("numeros");
+var lista = [];
+var contador = [];
 
 number.onkeyup = function(e){
 	if(e.which == 13) {
+		var N = parseInt(number.value); 
+		if(N < 1 || N > 100) {
+			alert("unicamente numeros del 1 al 100");
+			number.value = "";
+		}
+		else {
+			lista.push(number.value);
+			number.value = "";
+		}
+	}
+}
+
+btnAdd.onclick = function(){
+	var N = parseInt(number.value); 
+	if(N < 1 || N > 100) {
+		alert("unicamente numeros del 1 al 100");
+		number.value = "";
+	}
+	else {
 		lista.push(number.value);
 		number.value = "";
 	}
-=======
-txtnombres = document.getElementById("number");
-btnAdd = document.getElementById("agregar");
-btnHist = document.getElementById("hist");
+}
 
-
-
-
-function myFunction(event) {
-	var x = event.which || event.KeyCode;
-	document.getElementById("demo").innerHTML = "The Unicode value is: " + x;
->>>>>>> origin/master
+btnHist.onclick = function(){
+	
 }
